3636ea612a5d7bd42192708d71c79181ea7cc686
AJ Steinhauser <ajsteinhauser11@gmail.com>
2022-08-20 17:11:33 -0400

    Minor fixes for deployment

--- setup.py
+++ setup.py
@@ -2,13 +2,10 @@
 from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.read()
-
 
 setup(
     name = 'LUASprites',
-    version = '0.0.1',
+    version = '0.0.12',
     author = 'AJ Steinhauser',
     author_email = 'ajsteinhauser11@gmail.com',
     license = 'MIT License',
@@ -17,7 +14,14 @@ setup(
     long_description_content_type = "text/markdown",
     url = 'https://github.com/AJSteinhauser/LUASprites',
     packages = find_packages(),
-    install_requires = [requirements],
+    install_requires = [
+        'certifi==2022.6.15',
+        'charset-normalizer==2.1.1',
+        'idna==3.3',
+        'Pillow==9.2.0',
+        'requests==2.28.1',
+        'urllib3==1.26.11'
+    ],
     python_requires='>=3.7',
     classifiers=[
         "Programming Language :: Python :: 3.8",
